dfbcbeefc4030be2090fbf0d3f575e19a6f95a17
Han <79167052+hannert@users.noreply.github.com>
2025-03-22 18:59:57 -0400

    Update public-routes.spec.ts
    Two tests added for login: successful and unsuccessful

--- backend/tests/public-routes.spec.ts
+++ backend/tests/public-routes.spec.ts
@@ -1,17 +1,32 @@
 import { expect, test } from '@playwright/test';
 
-test('test', async ({ page }) => {
-  await page.goto('/login');
-  await expect(page.locator('body')).toContainText('Hello =)');
-});
-
-test('test1', async ({ request }) => {
-  const response = await request.post(``, {
+// Test for a successful (200) status code user login with authorization tokens set in response
+test('Login-Successful', async ({ request }) => {
+  const response = await request.post(`/auth/login`, {
     data: {
       username: 'tester',
       password: '123'
     }
   });
+  expect(response.ok()).toBeTruthy();
+
+  // Check if cookies are trying to be set in response
+  const headers = await response.headers();
+  expect(headers['set-cookie']).toContain('spotify_accessToken');
+  expect(headers['set-cookie']).toContain('spotify_refreshToken');
   
 });
+
+// Test for unsucessful (400) status code user login. (User does not exist)
+test('Login-Unsuccessful', async ({ request }) => {
+  const response = await request.post(`/auth/login`, {
+    data: {
+      username: 'User_That_Doesnt_Exist',
+      password: 'IncorrectPassword'
+    }
+  });
+  expect(response.status()).toBe(400);
+
+});
+
 
